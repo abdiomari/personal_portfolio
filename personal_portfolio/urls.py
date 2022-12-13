e7ea99080a77e8cb6617bdbdfdfbd252fd34fee4
Abdi Omari <74247785+abdiomari@users.noreply.github.com>
2022-12-13 17:58:55 +0300

    bonita website coming soon page

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -18,7 +18,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include('helloworld.urls')),
+     path('', include('pages.urls')),
    path("projects/", include("projects.urls")),
    path("blog/", include("blog.urls")),
 ]
